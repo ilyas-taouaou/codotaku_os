098caa5736ca575c664d84a87cabfdaf9382746b
ilyas-taouaou <ilyastaouaou@my.uopeople.edu>
2024-03-23 03:00:19 +0000

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,20 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    (0..100).for_each(|i| print!("{i} "));
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{info}");
     loop {}
 }
